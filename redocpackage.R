5db16d3714459e4b7d8b7328bc9ffceff55d8432
winterwang <abelardccwang@gmail.com>
2021-01-26 13:29:14 +0900

    comment changes from SA

--- redocpackage.R
+++ redocpackage.R
@@ -13,6 +13,7 @@ dedoc(redoc_example_docx())
 # 2. share docx to coauthors
 # 3. coauthors send back with docx file commented or modified
 # 4. convert modified docx back to rmd with redoc
-dedoc("Diabetes_CA_paper_LP.docx", track_changes = "all", overwrite = TRUE) # worked
-# 5. compare diff between rmd
-redoc_diff("Diabetes_CA_paper_LP.docx")
+dedoc("filefromcoauthor.docx", track_changes = "all", overwrite = TRUE) # worked
+# 5. compare diff between rmd　# to view the changes within Rstudio (under Ubuntu)
+redoc_diff("filefromecoauthor.docx")
+# 6. make changes according to the comments in rmd or docx?
