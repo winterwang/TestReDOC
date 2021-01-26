9939597b540ff2ea618cd829db11e229539aa59f
winterwang <abelardccwang@gmail.com>
2021-01-26 12:00:17 +0900

    convert luigi work back to RMD worked

--- redocpackage.R
+++ redocpackage.R
@@ -1,3 +1,18 @@
 library(redoc)
 print(basename(redoc_example_docx()))
 dedoc(redoc_example_docx())
+
+
+############################################################
+#                                                          #
+#                        work flow                         ####
+#                                                          #
+############################################################
+
+# 1. write my own rmd use redoc package to convert it to docx
+# 2. share docx to coauthors
+# 3. coauthors send back with docx file commented or modified
+# 4. convert modified docx back to rmd with redoc
+dedoc("Diabetes_CA_paper_LP.docx", track_changes = "all", overwrite = TRUE) # worked
+# 5. compare diff between rmd
+redoc_diff("Diabetes_CA_paper_LP.docx")
