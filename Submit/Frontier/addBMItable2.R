a3b57953e8aa566d713cc356930892f9dfc1d3ae
winterwang <abelardccwang@gmail.com>
2021-05-24 18:18:42 +0900

    prog added for reading bmi wst

--- Submit/Frontier/addBMItable2.R
+++ Submit/Frontier/addBMItable2.R
@@ -1,6 +1,3 @@
-# setwd("/home/wangcc-me/Downloads/UKDA-6533-stata11_se/stata11_se/") # on Ubuntu
-
-
 
 library(epiDisplay)
 library(plyr)
@@ -13,3 +10,14 @@ library(tidyverse)
 library(haven)
 
 
+
+y14bmi <- read_csv("dat/y14bmi.csv", col_types = cols(wstval = col_number(), 
+                                                      bmival = col_number()))
+y56bmi <- read_csv("dat/y56bmi.csv", col_types = cols(wstval = col_number(), 
+                                                      bmival = col_number()))
+y78bmi <- read_csv("dat/y78bmi.csv", col_types = cols(wstval = col_number(), 
+                                                      bmival = col_number()))
+y9bmi <- read_csv("dat/y9bmi.csv", col_types = cols(wstval = col_number(), 
+                                                      bmival = col_number()))
+
+NDNS_bmi <- rbind(y14bmi, y56bmi, y78bmi, y9bmi)
---
+++ dat/exportBMI.do
@@ -0,0 +1,15 @@
+use "/Users/wangcc.me/Downloads/TestReDOC/dat/ndns_rp_yr1-4a_indiv_uk.dta"
+outsheet seriali wstval bmival using y14bmi.csv , comma replace
+
+
+use "/Users/wangcc.me/Downloads/TestReDOC/dat/ndns_rp_yr5-6a_indiv.dta"
+
+outsheet seriali wstval bmival using y56bmi.csv , comma replace
+
+
+use "/Users/wangcc.me/Downloads/TestReDOC/dat/ndns_rp_yr7-8a_indiv.dta"
+outsheet seriali wstval bmival using y78bmi.csv , comma replace
+
+
+use "/Users/wangcc.me/Downloads/TestReDOC/dat/ndns_rp_yr9a_indiv.dta"
+outsheet seriali wstval bmival  using y9bmi.csv , comma replace
