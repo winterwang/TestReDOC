6ec4b243e9053123ab50ab032a5aaffa6f72e7d8
winterwang <abelardccwang@gmail.com>
2021-05-24 18:14:18 +0900

    updated addbmi to table programe

---
+++ Submit/Frontier/addBMItable2.R
@@ -0,0 +1,15 @@
+# setwd("/home/wangcc-me/Downloads/UKDA-6533-stata11_se/stata11_se/") # on Ubuntu
+
+
+
+library(epiDisplay)
+library(plyr)
+library(dplyr)
+library(tidyverse)
+
+
+# Read the data into memory -----------------------------------------------
+
+library(haven)
+
+
